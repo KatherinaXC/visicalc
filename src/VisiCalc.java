c6690c24225474e9261d609129894bad6093d8d6
Winterstorm <Winterstorm@M400>
2014-12-02 21:22:49 -0800

    some type of incremented progress i really don't know gah >.<

--- src/VisiCalc.java
+++ src/VisiCalc.java
@@ -1,11 +1,24 @@
 public class VisiCalc {
 
-    public VisiCalc(int rows, int cols) {
-        
+    public VisiCalc(int cols, int rows) {
+    }
+
+    public VisiCalc() {
+        //calls other constructor with default values 7 and 20
+        this(7, 20);
+    }
+
+    public String calc(String input) {
+    }
+    
+    public String getValue(String location) {
+    }
+
+    public String toString() {
     }
 
     public static String getHelp() {
-        //TODO
+        //TODO finish this
         return "Help text here.";
     }
 }
